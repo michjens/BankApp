0f07cc15b73b287893f18e88dca54dc1bc8d7995
michjens <michael.diksi@hotmail.com>
2019-06-12 00:21:39 +0200

    final push

--- app/src/main/java/com/example/bankapp/Activities/LoginActivity.java
+++ app/src/main/java/com/example/bankapp/Activities/LoginActivity.java
@@ -1,25 +1,44 @@
 package com.example.bankapp.Activities;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.Toast;
+import android.widget.TextView;
 
+import com.example.bankapp.Helpers.CurrentUser;
 import com.example.bankapp.R;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
+import org.w3c.dom.Text;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.example.bankapp.Helpers.Utility.*;
+
+
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText userMail, userPassword;
-    private Button btnLogin, forgotPass, btnReg;
+    EditText userMail, userPassword, nemIDpassword;
+    TextView nemID;
+    Button btnLogin, forgotPass, btnReg, btnConfirm;
+    HashMap userDetails;
     private ProgressBar loginProg;
     private FirebaseAuth mAuth;
 
@@ -42,21 +61,19 @@ public class LoginActivity extends AppCompatActivity {
                 final String mail = userMail.getText().toString();
                 final String password = userPassword.getText().toString();
 
-                if(mail.isEmpty() || password.isEmpty()){
-                    showMessage("Please verify all fields");
+                if (mail.isEmpty() || password.isEmpty()) {
+                    showMessage(getApplicationContext(), "Please verify all fields");
+                } else {
+                    buildAlertDialog(mail, password);
 
-                }else{
-                    login(mail, password);
-
-                }
-            }
+            }}
         });
 
         btnReg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent reg = new Intent(getApplicationContext(), RegisterActivity.class);
-                startActivity(reg);
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -64,13 +81,13 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (userMail.getText().toString().isEmpty()) {
-                    showMessage("Enter Email");
+                    showMessage(getApplicationContext(), "Enter Email");
                 } else {
                     FirebaseAuth.getInstance().sendPasswordResetEmail(userMail.getText().toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
-                            if(task.isSuccessful()){
-                                showMessage("Email sent");
+                            if (task.isSuccessful()) {
+                                showMessage(getApplicationContext(), "Email sent");
                             }
                         }
                     });
@@ -80,16 +97,70 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
+    private void buildAlertDialog(final String mail, final String password) {
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(LoginActivity.this);
+        View mView = getLayoutInflater().inflate(R.layout.dialog_login, null);
+        nemIDpassword = mView.findViewById(R.id.nemIDpassword);
+        nemID = mView.findViewById(R.id.nemID);
+        btnConfirm = mView.findViewById(R.id.btnConfirm);
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (nemIDpassword.getText().toString().isEmpty()) {
+                    showMessage(getApplicationContext(), "Please verify all fields");
+
+                }else{
+                    login(mail, password);
+                }
+            }
+        });
+        mBuilder.setView(mView);
+        AlertDialog dialog = mBuilder.create();
+        dialog.show();
+
+    }
+
     private void login(String mail, String password) {
+
         mAuth.signInWithEmailAndPassword(mail, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
+                if (task.isSuccessful()) {
                     loginProg.setVisibility(View.INVISIBLE);
                     btnLogin.setVisibility(View.VISIBLE);
-                    updateUI();
-                }else{
-                    showMessage("Wrong Email and/or Password");
+                    DocumentReference mDocRef = FirebaseFirestore.getInstance().document("Users/" + mAuth.getCurrentUser().getDisplayName());
+                    mDocRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                        @Override
+                        public void onSuccess(DocumentSnapshot documentSnapshot) {
+                            if (documentSnapshot.exists()) {
+                                if ((long) documentSnapshot.get(nemID.getText().toString()) == Long.parseLong(nemIDpassword.getText().toString())) {
+                                    Intent successfulLogin = new Intent(getApplicationContext(), HomeActivity.class);
+
+                                    CurrentUser currentUser = new CurrentUser(documentSnapshot.getString("name"),
+                                            documentSnapshot.getString("age"),
+                                            documentSnapshot.getString("Zipcode"),
+                                            documentSnapshot.getString("mail"),
+                                            documentSnapshot.getString("Budget"),
+                                            documentSnapshot.getString("Default"),
+                                            documentSnapshot.getString("Business"),
+                                            documentSnapshot.getString("Savings"),
+                                            documentSnapshot.getString("Pension"),
+                                            documentSnapshot.get("5632").toString());
+
+
+                                    successfulLogin.putExtra("currentUser", currentUser);
+                                    startActivity(successfulLogin);
+                                } else {
+                                    showMessage(getApplicationContext(), "Verify NemID key");
+                                }
+
+                            }
+
+                        }
+                    });
+
+                } else {
+                    showMessage(getApplicationContext(), "Wrong Email, Password or NemID key");
                     loginProg.setVisibility(View.INVISIBLE);
                     btnLogin.setVisibility(View.VISIBLE);
                 }
@@ -98,16 +169,8 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
-    private void updateUI() {
-        Intent home = new Intent(getApplicationContext(), HomeActivity.class);
-        startActivity(home);
-    }
-
-    private void showMessage(String msgIn) {
-        Toast.makeText(getApplicationContext(), msgIn, Toast.LENGTH_SHORT).show();
-    }
-
     private void init() {
+        userDetails = new HashMap<String, String>();
         userMail = findViewById(R.id.logMail);
         userPassword = findViewById(R.id.logPass);
         btnLogin = findViewById(R.id.logBtn);
@@ -115,8 +178,6 @@ public class LoginActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         forgotPass = findViewById(R.id.loginForgot);
         btnReg = findViewById(R.id.btnReg);
-
-
 
 
     }
--- app/src/main/java/com/example/bankapp/Activities/RegisterActivity.java
+++ app/src/main/java/com/example/bankapp/Activities/RegisterActivity.java
@@ -1,29 +1,41 @@
 package com.example.bankapp.Activities;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.Toast;
 
+import com.example.bankapp.Helpers.CurrentUser;
 import com.example.bankapp.R;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.example.bankapp.Helpers.Utility.*;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private EditText userEmail, userPassword, userPassword2, userName;
+    private EditText userEmail, userPassword, userPassword2, userName, userAge, userZipcode;
     private ProgressBar loadingProg;
-    private Button registerBtn, loginBtn;
+    private Button registerBtn;
     private FirebaseAuth mAuth;
+    CurrentUser currentUser;
 
 
     @Override
@@ -43,15 +55,50 @@ public class RegisterActivity extends AppCompatActivity {
                 loadingProg.setVisibility(View.VISIBLE);
                 final String name = userName.getText().toString();
                 final String email = userEmail.getText().toString();
+                final String age = userAge.getText().toString();
+                final String zipcode = userZipcode.getText().toString();
                 final String password = userPassword.getText().toString();
                 final String password2 = userPassword2.getText().toString();
+                DocumentReference mDocRef = FirebaseFirestore.getInstance().document("Users/" + name);
+
 
                 if(name.isEmpty() || email.isEmpty() || password.isEmpty()|| password2.isEmpty() || !password.equals(password2)){
-                    showMessage("Please verify all fields");
+                    showMessage(getApplicationContext(),"Please verify all fields");
                     registerBtn.setVisibility(View.VISIBLE);
                     loadingProg.setVisibility(View.INVISIBLE);
                 }else{
+                    currentUser.setmName(name);
+                    currentUser.setmMail(email);
+                    currentUser.setmAge(age);
+                    currentUser.setmZipcode(zipcode);
+                    currentUser.setmBudget("true");
+                    currentUser.setmDefault("true");
+                    currentUser.setmBusiness("false");
+                    currentUser.setmSavings("false");
+                    currentUser.setmPension("false");
                     createUserAcc(email, name, password);
+                    Map<String, Object> userSave = new HashMap<>();
+                    userSave.put("name", name);
+                    userSave.put("email", email);
+                    userSave.put("age", age);
+                    userSave.put("Zipcode", zipcode);
+                    userSave.put("Budget", "true");
+                    userSave.put("Default", "true");
+                    userSave.put("Business", "false");
+                    userSave.put("Savings", "false");
+                    userSave.put("Pension", "false");
+
+                    mDocRef.set(userSave).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            showMessage(getApplicationContext(), "Data has been saved");
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            showMessage(getApplicationContext(), "Data was not saved");
+                        }
+                    });
                 }
 
             }
@@ -65,12 +112,12 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()){
-                    showMessage("User registration complete");
+                    showMessage(getApplicationContext(),"User registration complete");
                     updateInfo(name, mAuth.getCurrentUser());
 
 
                 }else{
-                    showMessage("User registration failed " + "\n" + task.getException().getMessage());
+                    showMessage(getApplicationContext(),"User registration failed " + "\n" + task.getException().getMessage());
                     registerBtn.setVisibility(View.VISIBLE);
                     loadingProg.setVisibility(View.INVISIBLE);
                 }
@@ -79,14 +126,15 @@ public class RegisterActivity extends AppCompatActivity {
 
     }
 
-    private void updateInfo(String name, FirebaseUser currentUser) {
+    private void updateInfo(String name, final FirebaseUser userReg) {
         UserProfileChangeRequest profileUpdate = new UserProfileChangeRequest.Builder().setDisplayName(name).build();
-        currentUser.updateProfile(profileUpdate).addOnCompleteListener(new OnCompleteListener<Void>() {
+        userReg.updateProfile(profileUpdate).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
                 if(task.isSuccessful()){
-                    showMessage("User registration complete");
-                    updateUI();
+                    showMessage(getApplicationContext(), "User registration complete");
+                    updateUI(RegisterActivity.this, HomeActivity.class, currentUser);
+
 
 
                 }
@@ -95,23 +143,19 @@ public class RegisterActivity extends AppCompatActivity {
 
     }
 
-    private void updateUI() {
-        Intent home = new Intent(getApplicationContext(), HomeActivity.class);
-        startActivity(home);
-    }
-
-    private void showMessage(String msgIn) {
-        Toast.makeText(getApplicationContext(), msgIn, Toast.LENGTH_LONG).show();
-    }
 
     private void init() {
         userEmail = findViewById(R.id.regMail);
         userPassword = findViewById(R.id.regPass);
         userPassword2 = findViewById(R.id.regPass2);
         userName = findViewById(R.id.regName);
+        userAge = findViewById(R.id.regAge);
+        userZipcode = findViewById(R.id.regZipcode);
         loadingProg = findViewById(R.id.progressBar);
         registerBtn = findViewById(R.id.regBtn);
         mAuth = FirebaseAuth.getInstance();
-        loginBtn = findViewById(R.id.logBtn);
+
+
+
     }
 }
